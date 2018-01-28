2de6c0802c6daa74fd310afbdce4db9d67acca79
DariaBaklanova <dbaklanova0@gmail.com>
2018-01-28 17:03:28 -0500

    Add files via upload

---
+++ OnlinePlaylistApplication/Models/DesignModelClasses.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+// new...
+using System.ComponentModel.DataAnnotations;
+
+namespace Assignment7.Models
+{
+    // Add your design model classes below
+
+    // Follow these rules or conventions:
+
+    // To ease other coding tasks, the name of the 
+    //   integer identifier property should be "Id"
+    // Collection properties (including navigation properties) 
+    //   must be of type ICollection<T>
+    // Valid data annotations are pretty much limited to [Required] and [StringLength(n)]
+    // Required to-one navigation properties must include the [Required] attribute
+    // Do NOT configure scalar properties (e.g. int, double) with the [Required] attribute
+    // Initialize DateTime and collection properties in a default constructor
+
+    public class RoleClaim
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string Name { get; set; }
+    }
+
+    // ############################################################
+    // Genre
+
+    public class Genre
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string Name { get; set; }
+    }
+
+    // ############################################################
+    // Artist
+
+    public class Artist
+    {
+        public Artist()
+        {
+            BirthName = "";
+            BirthOrStartDate = DateTime.Now.AddYears(-20);
+            Albums = new List<Album>();
+            MediaItems = new List<MediaItem>();
+        }
+
+        public int Id { get; set; }
+
+        // For an individual, can be birth name, or a stage/performer name
+        // For a duo/band/group/orchestra, will typically be a stage/performer name
+        [Required, StringLength(100)]
+        public string Name { get; set; }
+
+        // For an individual, a birth name
+        [StringLength(100)]
+        public string BirthName { get; set; }
+
+        // For an individual, a birth date
+        // For all others, can be the date the artist started working together
+        public DateTime BirthOrStartDate { get; set; }
+
+        // Get from Apple iTunes Preview, Amazon, or Wikipedia
+        [Required, StringLength(200)]
+        public string UrlArtist { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+
+        [StringLength(10000)]
+        public string Profile { get; set; }
+
+        // User name who looks after this artist
+        [Required, StringLength(200)]
+        public string Executive { get; set; }
+
+        public ICollection<Album> Albums { get; set; }
+        public ICollection<MediaItem> MediaItems { get; set; }
+    }
+
+    // ############################################################
+    // Album
+
+    public class Album
+    {
+        public Album()
+        {
+            ReleaseDate = DateTime.Now;
+            Artists = new List<Artist>();
+            Tracks = new List<Track>();
+        }
+
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string Name { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        // Get from Apple iTunes Preview, Amazon, or Wikipedia
+        [Required, StringLength(200)]
+        public string UrlAlbum { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+
+        // User name who looks after the album
+        [Required, StringLength(200)]
+        public string Coordinator { get; set; }
+
+        public ICollection<Artist> Artists { get; set; }
+
+        public ICollection<Track> Tracks { get; set; }
+    }
+
+    // ############################################################
+    // Track
+
+    public class Track
+    {
+        public Track()
+        {
+            Albums = new List<Album>();
+        }
+
+        public int Id { get; set; }
+
+        [Required, StringLength(200)]
+        public string Name { get; set; }
+
+        // Simple comma-separated string of all the track's composers
+        [Required, StringLength(500)]
+        public string Composers { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+
+        // User name who added/edited the track
+        [Required, StringLength(200)]
+        public string Clerk { get; set; }
+
+        [StringLength(200)]
+        public string AudioContentType { get; set; }
+        public byte[] Audio { get; set; }
+
+        public ICollection<Album> Albums { get; set; }
+    }
+
+    // ############################################################
+    // MediaItem
+    public class MediaItem
+    {
+        public MediaItem()
+        {
+            Timestamp = DateTime.Now;
+
+            long i = 1;
+            foreach (byte b in Guid.NewGuid().ToByteArray())
+            {
+                i *= ((int)b + 1);
+            }
+            StringId = string.Format("{0:x}", i - DateTime.Now.Ticks);
+        }
+
+        public int Id { get; set; }
+
+        // Brief descriptive caption
+        [Required, StringLength(100)]
+        public string Caption { get; set; }
+
+        // Media item
+        public byte[] Content { get; set; }
+
+        [StringLength(200)]
+        public string ContentType { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        // For the generated identifier
+        [Required, StringLength(100)]
+        public string StringId { get; set; }
+    }
+
+
+}
